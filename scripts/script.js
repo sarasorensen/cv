09d409c48ca4e294741b0c4ec53a8688dcdf6767
Sara Sørensen <sarasorensen@Saras-MacBook-Air.local>
2020-10-13 12:49:36 +0200

    my cv website

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,7 @@ const handleClick = (event) => {
     document.addEventListener("click", offClick);
   }
 };
+
 toggleButton.addEventListener("click", handleClick);
 
 //menu change color
@@ -25,34 +26,23 @@ window.onscroll = function () {
     document.documentElement.scrollTop >= 80
   ) {
     myNav.classList.add("scroll");
-    topBtn.style.display = "none";
   } else {
     myNav.classList.remove("scroll");
-    topBtn.style.display = "none";
   }
 };
-
-//Back to top button
-function topButtonFunction() {
-  let body = document.body;
-  let html = document.documentElement;
-  body.scrollTop = 0;
-  html.scrollTop = 0;
-}
-
-const topBtn = document.getElementById("topButton");
-topBtn.addEventListener("click", topButtonFunction);
 
 let btn = document.getElementById("CTA");
 
 btn.addEventListener("click", function () {
   window.open("mailto:sarasorensen97@hotmail.com");
 });
-//scroll to resume
+//scroll to projects
 let button = document.getElementById("CTA2");
 
 button.addEventListener("click", function (e) {
-  document.getElementById("resume").scrollIntoView({ block: "center" });
+  document
+    .getElementById("projects-disp")
+    .scrollIntoView({ block: "center", behavior: "smooth" });
   e.stopPropagation();
 });
 
@@ -62,35 +52,3 @@ function NewTab(url) {
 
   win.focus();
 }
-
-const projects = [
-  {
-    link:
-      "http://sarasorensen.com/2020-09-13-semesterproject2-saras%C3%B8rensen%20-kopi/index.html",
-    src: "images/got.png",
-    alt: "A website",
-    id: "got",
-  },
-  {
-    link: "http://sarasorensen.com/examFebruary/index.html",
-    src: "images/space.png",
-    alt: "A website",
-    id: "space",
-  },
-];
-
-var displayProject = document.querySelector(".columnGa");
-
-function render() {
-  displayProject.innerHTML = "";
-
-  projects.forEach(function (object) {
-    displayProject.innerHTML += `
-                                  <div class="flex-box">
-                                  <img class="img-project" src=${object.src} alt=${object.alt} id=${object.id}>
-                                  <button type="button" class="projectBtn" onclick="NewTab(('${object.link}'))"> View Page </button>
-                                  </div>`;
-  });
-}
-
-render();
